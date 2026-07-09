89f2bcae1c89add4c07f35462303adc4cbcb92d5
Nanguti <dr.nanguti@gmail.com>
2026-07-09 13:56:09 +0300

    Fix nullable password field when seeding users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,7 +20,9 @@ class DatabaseSeeder extends Seeder
 
         User::firstOrCreate(
             ['email' => 'g.nanguti@gmail.com'],
-            ['name' => 'Nanguti - SA']
+            ['name' => 'Nanguti - SA',
+             'password' => Hash::make('password'),
+            ]
         );
 
         $this->call([
